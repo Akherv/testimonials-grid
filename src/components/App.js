48371f17e859c5b8c6e451842d9ed8230b4d5a47
Akherv <51315773+Akherv@users.noreply.github.com>
2021-09-30 20:28:07 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import '../styles/App.css';
+import Testimonial from './Testimonial';
+
+
+function App() {
+  return (
+    <div className="App">
+    <Testimonial />
+    </div>
+  );
+}
+
+export default App;
